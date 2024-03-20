f139e951c2431519fb51d67128a5c5bf0e22f968
H-LAP-00022\kiet.vh <kiet.vh@briswell-vn.com>
2024-03-20 15:54:50 +0700

    refs #126182 Research validation Server_Side fix digist input

--- app/Http/Requests/ValidationServerRequest.php
+++ app/Http/Requests/ValidationServerRequest.php
@@ -25,7 +25,7 @@ class ValidationServerRequest extends FormRequest
             'requiredInput' => 'required',
             'maxMinLengthInput' => 'required|min:8|max:20',
             'emailInput' => 'required|email',
-            'digitsInput' => 'required|digits',
+            'digitsInput' => 'required|digits:5',
             'numberInput' => 'required|numeric',
             'dateInput' => 'required|date_format:d/m/Y',
         ];
