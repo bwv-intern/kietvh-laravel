2acc17ac91daffdf56b8e1e47a523a044053613c
H-LAP-00022\kiet.vh <kiet.vh@briswell-vn.com>
2024-03-20 08:23:26 +0700

    refs #126181 Research Laravel 10.x (Routing)

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route Auth
+Route::get('/auth/login',  [AuthController::class, 'login'])->name('login');
+Route::post('/auth/login',  [AuthController::class, 'doLogin']);
+Route::get('/auth/logout',[AuthController::class, 'doLogOut'])->name('logOut');
+
+// Route Admin
+Route::prefix('admin')->middleware(['auth'])->group(function () {
+    Route::get('/',  [AdminController::class, 'index'])->name('index');
+
+    Route::prefix('category')->group(function () {
+        Route::get('/', [CategoryController::class, 'index'])->name('category.index');
+        Route::get('/add', [CategoryController::class, 'getAdd']);
+        Route::post('/add', [CategoryController::class, 'add']);
+        Route::get('/edit/{id}', [CategoryController::class, 'getEdit'])->name('category.gedit');
+        Route::post('/edit/', [CategoryController::class, 'postEdit'])->name('category.edit');
+        Route::get('/delete/{id}', [CategoryController::class, 'deleteByID']);
+    });
+
+    Route::prefix('product')->group(function () {
+        Route::get('/', [ProductController::class,'index'])->name('admin.product.index');
+        Route::get('/add', [ProductController::class,'getAdd']);
+        Route::post('/add', [ProductController::class,'add']);
+        Route::get('/edit/{id}', [ProductController::class,'getEdit'])->name('admin.product.gedit');
+        Route::post('/edit', [ProductController::class,'edit']);
+        Route::get('/delete/{id}', [ProductController::class,'delete']);
+    });
+
 });
