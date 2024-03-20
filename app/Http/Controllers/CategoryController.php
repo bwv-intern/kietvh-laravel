cf8dbf6de2cba3daf82d4b3e6d567b2387d80114
H-LAP-00022\kiet.vh <kiet.vh@briswell-vn.com>
2024-03-20 09:22:43 +0700

    refs #126181 Research Laravel 10.x (Eloquent ORM)

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,26 +9,55 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     public function index(){
-        return "index";
+
+        $categories = Category::all();
+        return view('admin.category.index',compact('categories'));
+
     }
 
     public function getEdit($id){
-        return "getEdit";
+        $category = Category::find($id);
+        return view('admin.category.edit',compact('category'));
+
     }
 
     public function postEdit(Request $request){
-        return "postEdit";
+        $idCategory = $request -> idCategory;
+        $nameCategory =  $request -> nameCategory;
+
+        $category = Category::findOrFail($idCategory);
+
+        $category->name = $nameCategory;
+        $category->save();
+
+        return redirect()->route('category.gedit', ['id' => $idCategory])->with('success', 'Category đã được cập nhật thành công');
+
     }
 
     public function getAdd(){
-        return "getAdd";
+        return view('admin.category.add');
     }
 
     public function add(Request $request){
-        return "add";
+        $nameCategory = $request ->nameCategory;
+        $category = new Category();
+        $category -> name = $nameCategory;
+        $category -> save();
+
+        return redirect()->route('category.gedit', ['id' => $category -> id])->with('success', 'Danh mục đã được thêm thành công');
     }
 
     public function deleteByID($id){
-        return "deleteByID";
+        try {
+
+            $category = Category::findOrFail($id);
+
+            $category->delete();
+
+            return redirect()->route('category.index')->with('success', 'Danh mục đã được xóa thành công');
+        } catch (\Exception $e) {
+
+            return redirect()->route('category.index')->with('error', 'Đã xảy ra lỗi khi xóa Danh mục');
+        }
     }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,37 +2,78 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return "index";
+        $products = Product::all();
+        return view('admin.product.index', compact('products'));
     }
 
 
     public function add(Request $request)
     {
-        return "add";
+        $product = new Product();
+        $product->name = $request->input('productName');
+        $product->price = $request->input('productPrice');
+        $product->id_category = $request->category_id;
+        $product->quantity = $request->productQuantity;
+        $product->description = $request->input('productDescription');
+        $product->save();
+
+        return redirect()->route('admin.product.index')->with('success', 'Thêm sản phẩm thành công!');
     }
 
     public function getAdd()
     {
-        return "getAdd";
+        $categories = Category::all();
+        return view('admin.product.add', compact('categories'));
     }
 
     public function getEdit($id)
     {
-        return "getEdit";
+        $product = Product::find($id);
+        $categories = Category::all();
+        return view('admin.product.edit', compact('product', 'categories'));
     }
 
-    public function edit(ProductRequest $request)
+    public function edit(Request $request)
     {
-        return "edit";
+        $productId = $request->input('productID');
+        $productName = $request->input('productName');
+        $categoryId = $request->input('category_id');
+        $productPrice = $request->input('productPrice');
+        $productQuantity = $request->input('productQuantity');
+        $productDescription = $request->input('productDescription');
+
+        $product = Product::findOrFail($productId);
+
+        $product->name = $productName;
+        $product->id_category = $categoryId;
+        $product->price = $productPrice;
+        $product->quantity = $productQuantity;
+        $product->description = $productDescription;
+
+        $product->save();
+
+        return redirect()->route('admin.product.gedit', ['id' => $productId])->with('success', 'sản phẩm đã được cập nhật thành công');
     }
     public function delete($id)
     {
-        return "delete";
+        $product = Product::find($id);
+
+        if (!$product) {
+            return redirect()->back()->with('error', 'Không tìm thấy sản phẩm để xóa.');
+        }
+
+        $product->delete();
+
+        return redirect()->route('admin.product.index')->with('success', 'Sản phẩm đã được xóa thành công.');
     }
 }
